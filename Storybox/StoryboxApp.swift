3fe2a42146998702c4d838336e505aa8714b20e7
Deniz Binay <deniz@bombig.net>
2024-04-24 19:44:15 +0200

    Initial Commit

---
+++ Storybox/StoryboxApp.swift
@@ -0,0 +1,17 @@
+//
+//  StoryboxApp.swift
+//  Storybox
+//
+//  Created by User on 24.04.24.
+//
+
+import SwiftUI
+
+@main
+struct StoryboxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            // WelcomeView()  // Assuming you've created a WelcomeView for the entry screen
+        }
+    }
+}
